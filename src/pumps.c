b99305b5a4895482a7cce0506628970ad9638614
chohj2614 <101245494+chohj2614@users.noreply.github.com>
2025-02-28 15:39:51 -0500

    changed to receiving pointers

--- src/pumps.c
+++ src/pumps.c
@@ -1,25 +1,26 @@
 #include "stm32f0xx.h"
 
-void togglpump(GPIO_TypeDef *port);
-void Toggle_Col(void);
+void togglpump(GPIO_TypeDef *port, int Curr_Pump);
+void Toggle_Col(int * Curr_Pump);
 
 
-int Curr_Pump = 8; //PB8 = output connected to first pump
+// int Curr_Pump = 8; //PB8 = output connected to first pump
 
 //Change current Pump
-void Toggle_Col(void){
-    Curr_Pump += 1;
-    if(Curr_Pump == 11){
-        Curr_Pump = 8;
+void Toggle_Col(int* Curr_Pump){
+    *Curr_Pump += 1;
+    if(*Curr_Pump == 11){
+        *Curr_Pump = 8;
     }
 }
-    
- void togglpump(GPIO_TypeDef *port) {
-    int temp = port->IDR & (1 << Curr_Pump);
-        //(temp == 0) means button pushed
-    if(!temp) {
-      GPIOB->BSRR = 1 << Curr_Pump; //set 0 to 1 at pin Curr_Pump
-    } else {
-      GPIOB->BRR = 1 << Curr_Pump; //set 1 to 0 at pin Curr_Pump
+
+
+void togglpump(GPIO_TypeDef *port, int Curr_Pump) {
+    int temp = port->ODR & (1 << Curr_Pump);
+
+    if(temp) { //if output is on
+        GPIOB->BRR = 1 << Curr_Pump; //set pin Curr_Pump to low
+    } else {    // if output is off
+        GPIOB->BSRR = 1 << Curr_Pump; //set pin Curr_Pump to high
     }
 }
